0fdf3c9d7c0e08b7a70ea23b6464ebcbc3285155
unknown <adam.lucak@infis.cz>
2025-04-29 08:23:17 +0200

    hotovo

---
+++ body.py
@@ -0,0 +1,11 @@
+skore = [1,2,9,7,10,11,20,16,24,56]
+for i in skore:
+    print(i)
+print(sum(skore))
+print(max(skore))
+print(min(skore))
+prumer = print(sum(skore))/10
+if prumer <50:
+    print("vyborny vykon")
+else:
+    print("muzete to priste zkusit lepe")
